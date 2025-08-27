2beac8e9c871c2fa99dce1d1d2f3ef253fde8762
conexon-amiga <mrkrolix63@gmail.com>
2025-08-27 15:51:19 +0300

    create array of colors vars

--- src/Food.java
+++ src/Food.java
@@ -6,20 +6,22 @@ public class Food {
     public float x;
     public float y;
     public boolean toBeDeleted;
+    public static Color red = new Color (255, 100, 100);
+    public static Color green = new Color(50, 150, 50);
+    public static Color blue = new Color(100, 150, 200);
+    public static Color yellow = new Color(50, 50, 255);
 
     public static Color[] COLOR = {
-            new Color(255, 100, 100), // eto morkovka
-            new Color(200, 200, 100),
-            new Color(50, 150, 50),
-            new Color(100, 150, 200),
-            new Color(50, 50, 255),
-            new Color(150, 50, 150)
+            red, // eto morkovka
+            green,
+            blue,
+            yellow 
     };
 
     public Food(float x, float y) {
         this.x = x;
         this.y = y;
-        this.type = (int)(Math.random() * 6);
+        this.type = (int)(Math.random() * COLOR.length);
         this.toBeDeleted = false;
     }
 
